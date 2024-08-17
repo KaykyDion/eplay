51acd95694c2b15ac05b2bb66dfc1ed34c9870b5
Kayky Dion <kayky.dion@gmail.com>
2024-08-17 17:24:57 -0300

    Criando componente de Tag e Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,27 @@
+import { ButtonContainer, ButtonLink } from './styles'
+
+type Props = {
+  type: 'button' | 'link'
+  title: string
+  to?: string
+  onClick?: () => void
+  children: string
+}
+
+const Button = ({ type, title, onClick, to, children }: Props) => {
+  if (type === 'button') {
+    return (
+      <ButtonContainer type="button" title={title} onClick={onClick}>
+        {children}
+      </ButtonContainer>
+    )
+  } else {
+    return (
+      <ButtonLink title={title} to={to as string}>
+        {children}
+      </ButtonLink>
+    )
+  }
+}
+
+export default Button
